1f306da1e0559c3c12331a7c37ed980db47b71c0
Jasvolin <shanin67@bk.ru>
2025-09-05 14:41:40 +0300

    добавлен таймер

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ const colors = [
   "#F28F58aa",
   "#D83D18aa",
 ];
-let n = getNumber(41, 1);
+let n = Math.floor(getNumber(61, 6) / 6) * 6;
+
 while (n--) {
   data.push(getNumber(101));
 }
 console.log(data);
 
 const box = document.querySelector(".box");
+box.innerHTML = '<div class = "card"></div>';
 for (let val of data) {
   box.innerHTML += `<div 
   class="card" 
@@ -23,3 +25,19 @@ for (let val of data) {
     colors[getNumber(colors.length - 1)]
   } ">${optimazeNumber(val)}</div>`;
 }
+function showTime(day = new Date(2026, 1, 7)) {
+  let now = new Date();
+  let d, h, m, s;
+  let time = Math.floor((day - now) / 1000);
+  m = Math.floor(time / 60);
+  s = time - m * 60;
+  h = Math.floor(m / 60);
+  m -= h * 60;
+  d = Math.floor(h / 24);
+  return `${d}д. ${h}ч. ${m} мин. ${s} сек.`;
+}
+console.log(showTime());
+
+setInterval(function () {
+  box.firstElementChild.innerText = `До окончания осталось ${showTime()}`;
+}, 1000);
